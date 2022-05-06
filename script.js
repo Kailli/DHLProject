5602aa53b241d98bef72044b0664b1d47c6f9c83
Kailli <kaillichaechae10@gmail.com>
2022-05-06 21:48:07 +0630

    DHL_FirstSeen

---
+++ script.js
@@ -0,0 +1,34 @@
+let frontUk=document.querySelector('#front-uk');
+document.querySelector('#flat-uk').onclick = () => {
+    frontUk.classList.toggle('active');
+}
+
+let frontVit=document.querySelector('#front-vit');
+document.querySelector('#flat-vit').onclick = () => {
+    frontVit.classList.toggle('active');
+}
+
+let frontUs=document.querySelector('#front-us');
+document.querySelector('#flat-us').onclick = () => {
+    frontUs.classList.toggle('active');
+}
+
+let frontChina=document.querySelector('#front-china');
+document.querySelector('#flat-china').onclick = () => {
+    frontChina.classList.toggle('active');
+}
+
+let frontNg=document.querySelector('#front-ng');
+document.querySelector('#flat-ng').onclick = () => {
+    frontNg.classList.toggle('active');
+}
+
+let frontKorea=document.querySelector('#front-korea');
+document.querySelector('#flat-korea').onclick = () => {
+    frontKorea.classList.toggle('active');
+}
+
+let frontJapan=document.querySelector('#front-japan');
+document.querySelector('#flat-japan').onclick = () => {
+    frontJapan.classList.toggle('active');
+}
